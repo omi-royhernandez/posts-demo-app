0572e7470634742e39058a2c35fdfa82bfa30ba1
Roy Stephen A. Hernandez <roy.hernandez@admins-MacBook-Pro.local>
2024-07-30 14:15:21 +0800

    Add react-router-dom package

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/c
 import { ThemeProvider } from '@mui/styles';
 import { createTheme } from '@mui/material';
 import Posts from './Posts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Create an HttpLink to the GraphQL Zero endpoint
 const httpLink = new HttpLink({
@@ -20,7 +21,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <ApolloProvider client={apolloClient}>
-        <Posts />
+        <Router>
+          {/* Entry point for routes */}
+          <Routes>
+            <Route path="/" element={<Posts />} />
+          </Routes>
+        </Router>
       </ApolloProvider>
     </ThemeProvider>
   );
